147ce7f3c73e0728e58b1b352affce7668132653
Fabian Senftleben <fabiansenftlebe@gmail.com>
2024-06-25 18:02:46 +0200

    Added Passive (Piezzo) Buzzer example

---
+++ Passiver-Buzzer/src/main.cpp
@@ -0,0 +1,57 @@
+// #################################################################################
+//          Source: https://esp32io.com/tutorials/esp32-button-piezo-buzzer
+// #################################################################################
+
+#include <Arduino.h>
+#include "notes.h"
+
+#define BUZZER_PIN 27
+
+int melody[] = {
+        NOTE_E5, NOTE_E5, NOTE_E5,
+        NOTE_E5, NOTE_E5, NOTE_E5,
+        NOTE_E5, NOTE_G5, NOTE_C5, NOTE_D5,
+        NOTE_E5,
+        NOTE_F5, NOTE_F5, NOTE_F5, NOTE_F5,
+        NOTE_F5, NOTE_E5, NOTE_E5, NOTE_E5, NOTE_E5,
+        NOTE_E5, NOTE_D5, NOTE_D5, NOTE_E5,
+        NOTE_D5, NOTE_G5
+};
+
+int noteDurations[] = {
+        8, 8, 4,
+        8, 8, 4,
+        8, 8, 8, 8,
+        2,
+        8, 8, 8, 8,
+        8, 8, 8, 16, 16,
+        8, 8, 8, 8,
+        4,
+};
+
+void buzzer();
+
+void setup() {
+    pinMode(BUZZER_PIN, OUTPUT);
+}
+
+void loop() {
+//    tone(BUZZER_PIN, 440, 200);
+//    noTone(BUZZER_PIN);
+    buzzer();
+    delay(3000);
+}
+
+void buzzer() {
+    int size = sizeof(noteDurations) / sizeof(int);
+
+    for (int thisNote = 0; thisNote < size; thisNote++) {
+        int noteDuration = 1000 / noteDurations[thisNote];
+        tone(BUZZER_PIN, melody[thisNote], noteDuration);
+
+        int pauseBetweenNotes = noteDuration * 1.30;
+        delay(pauseBetweenNotes);
+
+        noTone(BUZZER_PIN);
+    }
+}
--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 1. [Taster](./Taster)
 2. [E-Ink Basic UI](./E-Ink-Basic-UI)
 3. [Dallas 18B20 / DS18B20 Temperatursensor](./Dallas18B20)
+4. [Passiver Buzzer / Piezzo Buzzer](./Passiver-Buzzer)
